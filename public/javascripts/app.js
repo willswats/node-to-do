b1762d3c7dd869ccf85c9414887c02c07771950b
William Watson <williamstuwatson@gmail.com>
2021-07-26 16:34:46 +0100

    Fix mobile menu

---
+++ public/javascripts/app.js
@@ -0,0 +1,8 @@
+const mobileMenu = document.querySelector('#mobile-menu')
+const navMenu = document.querySelector('#nav-menu')
+
+const activateMobileMenu = () => {
+    mobileMenu.classList.toggle('cross')
+    navMenu.classList.toggle('drop-down')
+}
+mobileMenu.addEventListener('click', activateMobileMenu)
