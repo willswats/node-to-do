016fb0b50ac07de5b25410100cb017f07162d53c
William Watson <williamstuwatson@gmail.com>
2021-08-04 11:24:59 +0100

    Add fade and timeout to flash dismissal

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -27,11 +27,24 @@ const crossSuccess = document.getElementsByClassName('cross-success')
 const crossError = document.getElementsByClassName('cross-error')
 for (let i = 0, crossSuccessLength = crossSuccess.length; i < crossSuccessLength; i++) {
     crossSuccess[i].addEventListener("click", function () {
-        flashSuccess[i].classList.add('hide')
+        flashSuccess[i].classList.add('fade')
+        flashSuccess[i].addEventListener('transitionend', () => {
+            flashSuccess[i].classList.add('hide')
+        })
     })
+    setTimeout(() => {
+        flashSuccess[i].classList.add('fade')
+        flashSuccess[i].addEventListener('transitionend', () => {
+            flashSuccess[i].classList.add('hide')
+        })
+    }, 5000)
 }
+
 for (let i = 0, crossErrorLength = crossError.length; i < crossErrorLength; i++) {
     crossError[i].addEventListener("click", function () {
-        flashError[i].classList.add('hide')
+        flashError[i].classList.add('fade')
+        flashError[i].addEventListener('transitionend', () => {
+            flashError[i].classList.add('hide')
+        })
     })
 }
